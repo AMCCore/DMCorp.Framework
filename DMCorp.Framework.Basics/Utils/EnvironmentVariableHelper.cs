0d30e7027e6e6b2d19aadc1713f4c63cd5d84638
Albert Shtradman <info@dm.bike>
2026-03-31 18:21:01 +0300

    EnvironmentVariableHelper

---
+++ DMCorp.Framework.Basics/Utils/EnvironmentVariableHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DMCorp.Framework.Basics.Utils;
+
+public static class EnvironmentVariableHelper
+{
+    public static void DebugSet(this IConfiguration builder, string Name, string? EnvName = default)
+    {
+        Environment.SetEnvironmentVariable(string.IsNullOrWhiteSpace(EnvName) ? Name : EnvName, builder.GetValue<string>(Name));
+    }
+}
